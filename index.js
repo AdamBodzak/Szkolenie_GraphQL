01544ab4cc039853c4ee7d35f8d98c7941f45019
Adam Bodzak <bodzak.adam@gmail.com>
2021-02-17 14:55:41 +0100

    Add new data value randomDiceThrow

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const { ApolloServer, gql } = require("apollo-server");
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 const typeDefs = gql`
     type Query {
         greeting: String
         interestingUrls: [String]
+        randomDiceThrow: Int
     }
 `;
+function rootValue() {
+    const getRandomDiceThrow = (sides) => Math.ceil(Math.random() * sides);
+    const data = {
+        greeting: "Hello world!",
+        interestingUrls: ["https://kusrsrecta.pl", "https://64bits.com"],
+        randomDiceThrow: getRandomDiceThrow(6)
+    };
+    return data;
+};
 
-const data = {
-    greeting: "Hello world!",
-    interestingUrls: ["https://kusrsrecta.pl","https://64bits.com"]
-}
-
-const server = new ApolloServer({ typeDefs, rootValue: data, playground: true, introspection:true });
+const server = new ApolloServer({ typeDefs, rootValue, playground: true, introspection: true });
 
 server.listen({ port: PORT }).then((result) => console.log(result.url))
